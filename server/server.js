5a2ceb22f8660cea913522d4e539fdd14834d71f
Shin <sadiqmohammad201@gmail.com>
2026-09-23 19:02:33 +0530

    setup db connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const app = require('./app')
+const pool = require('./db/config')
 
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}...`)
 })
+
+
+const test_db_con = async () => {
+    try {
+        const result = await pool.query('SELECT NOW()')
+        console.log(`database connected: ${result.rows[0].now}`)
+    } catch (error) {
+        console.log('database connection failed: ', error)
+    }
+}
+
+test_db_con()
